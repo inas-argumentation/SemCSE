8b0057037ee2e995a4e7c862b30eb98d5581b4d7
Marc Brinner <marc@brinner.eu>
2025-07-18 11:10:51 +0200

    Update main.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from settings import set_save_name, set_encoder_checkpoint, metric
 
-if __name__ == '__main__':
-    set_save_name("SemCSE_run_test")
+def train_SemCSE():
+    set_save_name("SemCSE")
     set_encoder_checkpoint("KISTI-AI/Scideberta-full")
 
     # Train model
@@ -15,3 +15,25 @@ if __name__ == '__main__':
 
     from semantic_benchmark import evaluate_model_semantics
     evaluate_model_semantics(model, tokenizer, output_processing="last_hidden", metric=metric)
+
+def test_SemCSE_Euclidean():
+    from transformers import AutoTokenizer, AutoModel
+    tokenizer = AutoTokenizer.from_pretrained("CLAUSE-Bielefeld/SemCSE")
+    model = AutoModel.from_pretrained("CLAUSE-Bielefeld/SemCSE").to("cuda")
+
+    from semantic_benchmark import evaluate_model_semantics
+    evaluate_model_semantics(model, tokenizer, output_processing="last_hidden", metric="euclidean")
+
+def test_SemCSE_cosine():
+    from transformers import AutoTokenizer, AutoModel
+    tokenizer = AutoTokenizer.from_pretrained("CLAUSE-Bielefeld/SemCSE_cosine")
+    model = AutoModel.from_pretrained("CLAUSE-Bielefeld/SemCSE_cosine").to("cuda")
+
+    from semantic_benchmark import evaluate_model_semantics
+    evaluate_model_semantics(model, tokenizer, output_processing="last_hidden", metric="cosine")
+
+
+if __name__ == '__main__':
+    #train_SemCSE()
+    test_SemCSE_Euclidean()
+    test_SemCSE_cosine()
